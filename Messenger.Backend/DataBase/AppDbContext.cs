d64b225356b402ab679c81bc42d7453787bc1dbe
Bulisz <bulisz@freemail.hu>
2023-07-16 07:43:34 +0200

    back to .net6

--- Messenger.Backend/DataBase/AppDbContext.cs
+++ Messenger.Backend/DataBase/AppDbContext.cs
@@ -6,8 +6,8 @@ namespace Messenger.Backend.DataBase;
 
 public class AppDbContext : IdentityDbContext<ApplicationUser>
 {
-    public DbSet<Message> Messages { get; set; }
-    public DbSet<Group> Groups { get; set; }
+    public DbSet<Message> Messages { get; set; } = null!;
+    public DbSet<Group> Groups { get; set; } = null!;
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }

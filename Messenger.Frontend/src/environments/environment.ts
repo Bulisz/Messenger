dbe24f30a36f83ad4d32157d47c579db7b1fb40f
Bulisz <bulisz@freemail.hu>
2023-07-21 17:21:21 +0200

    iis deploy

--- Messenger.Frontend/src/environments/environment.ts
+++ Messenger.Frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrl: 'https://pn2000jan01-001-site1.ctempurl.com/messenger/',
-    hubUrl: 'https://pn2000jan01-001-site1.ctempurl.com/messengerhub',
+    apiUrl: 'http://86.101.113.227:81/messenger/',
+    hubUrl: 'http://86.101.113.227:81/messengerhub',
     clientId: '218311548705-pjuhjv6h1to4ip7ahp0q3vk35lhuo73r.apps.googleusercontent.com'
 };
